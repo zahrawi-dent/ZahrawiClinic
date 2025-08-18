d75bd4120ef00e2faecd42bcf6d2be01763378d8
MuhammadSwa <muhammadatefsw@protonmail.com>
2025-08-19 00:32:02 +0300

    add basic pwa setup

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,11 +3,49 @@ import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
   plugins: [
     tanstackRouter({
       target: 'solid',
       autoCodeSplitting: true,
     }),
-    solid(), tailwindcss()],
+    solid(),
+    tailwindcss(),
+    VitePWA({
+
+      includeAssets: ['dentist-svgrepo-com.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ZahrawiClinic Dental Management App',
+        short_name: 'ZahrawiClinic',
+        description: 'A dental management app',
+        theme_color: '#ffffff',
+        "icons": [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
+  ],
 })
